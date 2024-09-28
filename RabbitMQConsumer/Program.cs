9a9a9665ebac1396484222a775db9c8b92eeb246
Gazala Anjum <gazalak528@gmail.com>
2024-09-28 21:40:34 +0530

    Add project files.

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQConsumer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO");
+            var factory = new ConnectionFactory { Uri = new Uri("amqp://guest:guest@localhost:5672") };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("test-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            //Fair Dispatch
+            channel.BasicQos(prefetchSize:0, prefetchCount:1, global:false);
+            Console.WriteLine("Waiting for message");
+            var consumer = new EventingBasicConsumer(channel);
+
+            //Event Handler
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+               
+                if (message.Contains("exception"))
+                {
+                    Console.WriteLine("Error");
+                    channel.BasicReject(e.DeliveryTag, false);
+                  
+                    throw new Exception("Error in processing");
+                }
+
+                if (int.TryParse(message, out int delayTime))
+                {
+                    Thread.Sleep(delayTime * 1000);
+
+                }
+                Console.WriteLine($"Processed message {message}");
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+            channel.BasicConsume("test-queue", false, consumer);
+
+            Console.WriteLine("Enter to Exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQProducer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+namespace RabbitMQProducer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO");
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare("test-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            //var message = new { Name = "Producer", Message = "Hello!" };
+
+            string? message = null;
+            do 
+            {
+                Console.WriteLine("Please enter your message");
+                message = Console.ReadLine();
+                if (!string.IsNullOrEmpty(message))
+                    SendMessage(channel, message);
+            } while (!string.IsNullOrEmpty(message));
+
+           
+        }
+
+        static void SendMessage(IModel channel, string message)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish("", "test-queue", null, body);
+            Console.WriteLine($"Send Message {message}");
+        }
+    }
+    
+}
